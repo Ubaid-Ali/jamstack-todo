9e1a891109d866381568765710f52b7b5bb67d22
Ubaid-Ali <syedubaidali2020@gmail.com>
2021-05-28 03:31:22 +0500

    Added netlifyIdentity

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Button, Heading, Flex } from "theme-ui";
+import netlifyIdentity from "netlify-identity-widget";
 
 const Index = () => {
+  useEffect(() => {
+    netlifyIdentity.init();
+  });
+
   return (
     <Container>
       <Flex sx={{ flexDirection: "column", padding: 3 }}>
         <Heading as="h1">Hello World from Home</Heading>
-        <Button onClick={() => alert("Login!")} sx={{ marginTop: 2 }}>
+        <Button onClick={() => netlifyIdentity.open()} sx={{ marginTop: 2 }}>
           Login
         </Button>
       </Flex>

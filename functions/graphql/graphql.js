685a95475c2cf9f5e8129b597fabd8152bba63c1
Ubaid-Ali <syedubaidali2020@gmail.com>
2021-06-01 03:30:20 +0500

    enable cors for graphql api

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -49,4 +49,9 @@ const server = new ApolloServer({
   introspection: true,
 });
 
-exports.handler = server.createHandler();
+exports.handler = server.createHandler({
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
